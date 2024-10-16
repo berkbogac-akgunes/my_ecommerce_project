c7a106bc8d8ac5b37aa3cfb9538027c54e24feea
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-10-16 20:42:36 +0300

    shop card section done

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,18 +1,46 @@
-import menImage from "../assets/images/men.jpeg"
-
-export function ShopCard() {
+/* eslint-disable react/prop-types */
+export function ShopCard(props) {
+    const {cardInfo} = props
     return(
         <>
-        <div className ="relative">
-          <img
-          className ="h-[31rem] object-cover" 
-          src = {menImage}
-          title="men"
-          />
-          <button className = "absolute left-7 bottom-6 absolute bg-white py-2.5 px-16 font-bold text-lg">MEN</button>
-        </div>
-        {/* create 4 shop cards here and get images with prop drilling so we can change it easily next time if we want. */}
-        {/* same with product cards but just in the component we need just 1 product card. */}
+        <section className = "md:flex md:justify-center mt-12">
+          <div className ="relative mb-8 md:mr-8">
+            <img
+            className ="md:h-[31rem] md:w-[32rem] h-[31rem] object-cover"
+            src = {cardInfo[0].src}
+            title= {cardInfo[0].title}
+            />
+            <button className = "absolute left-7 bottom-6 absolute bg-white py-2.5 px-16 font-bold text-lg">{cardInfo[0].buttonTitle}</button>
+          </div>
+
+          <div className ="relative mb-8 md:mr-8">
+            <img
+            className ="md:h-[31rem] md:w-[15rem] h-[31rem] w-full object-cover" 
+            src = {cardInfo[1].src}
+            title= {cardInfo[1].title}
+            />
+            <button className = "md:px-8 md:left-4 md:text-base absolute left-7 bottom-6 absolute bg-white py-2.5 px-16 font-bold text-lg">{cardInfo[1].buttonTitle}</button>
+          </div>
+          <div className = "md:flex md:flex-col">
+            <div className ="relative mb-4">
+              <img
+              className ="md:h-[15rem] md:w-[15rem] h-[15rem] w-full object-cover" 
+              src = {cardInfo[2].src}
+              title= {cardInfo[2].title}
+              />
+              <button className = "md:px-4 md:left-4 md:text-base absolute left-7 bottom-6 absolute bg-white py-2.5 px-8 font-bold text-lg">{cardInfo[2].buttonTitle}</button>
+            </div>
+
+            <div className ="relative">
+              <img
+              className ="md:h-[15rem] md:w-[15rem] h-[15rem] w-full object-cover" 
+              src = {cardInfo[3].src}
+              title= {cardInfo[3].title}
+              />
+              <button className = "absolute left-7 bottom-6 absolute bg-white py-2.5 px-8 font-bold text-lg">{cardInfo[3].buttonTitle}</button>
+            </div>
+          </div>
+        </section>
         </>
     )
 }
--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,17 +1,17 @@
-import { ShopCard } from "@/components/ShopCard";
+import { ShopCardGroup } from "@/components/ShopCardGroup";
 
 export function PageContent() {
     return(
-        <>
-        <section className = "py-20 gap-y-12 mx-10">
+        <section className = "bg-[#FAFAFA]">
+        <div className = "py-20 gap-y-12 mx-10 md:mx-60">
             <div className = "flex flex-col text-center gap-y-2.5">
                 <h3 className = "mont font-bold text-2xl text-[#252B42]">{"EDITOR'S PICK"}</h3>
                 <p className = "font-normal text-[#737373]">Problems trying to resolve the conflict between </p>
             </div>
-            <div>
-                <ShopCard/>
+            <div className = "">
+                <ShopCardGroup/>
             </div>
+        </div>
         </section>
-        </>
     )
 }
