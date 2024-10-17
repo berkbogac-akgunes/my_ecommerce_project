51ceb771063b6b989f963583ae4d73d4d9cc5e01
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-10-17 18:10:57 +0300

    add mobile product cards

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+export function ProductCard(props) {
+    const {item} = props
+    return(
+        <>
+        <section className = "mt-12">
+          <div className ="flex flex-col text-center items-center">
+            <img
+            className ="h-[27rem] object-cover pb-4"
+            src = {item.src}
+            title= {item.title}
+            />
+            <div className = "pb-6">
+                <h5 className = "font-bold text-lg mb-2">{item.productTitle}</h5>
+                <p className = "font-bold text-[#737373] mb-3">{item.productExplanation}</p>
+                <div className = "flex text-center justify-center mb-3">
+                    <h5 className = "font-bold text-base mr-1.5 text-[#BDBDBD]">{item.oldPrice}</h5>
+                    <h5 className = "font-bold text-base text-[#23856D]">{item.newPrice}</h5>
+                </div>
+                <div className = "">
+                <span className="h-4 w-4 bg-[#23A6F0] rounded-full mr-1 inline-block"></span>
+                <span className="h-4 w-4 bg-[#23856D] rounded-full mr-1 inline-block"></span>
+                <span className="h-4 w-4 bg-[#E77C40] rounded-full mr-1 inline-block"></span>
+                <span className="h-4 w-4 bg-[#252B42] rounded-full inline-block"></span>
+                </div>
+            </div>
+          </div>
+        </section>
+        </>
+    )
+}
--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,7 +1,9 @@
+import { ProductCardsGroup } from "@/components/ProductCardsGroup";
 import { ShopCardGroup } from "@/components/ShopCardGroup";
 
 export function PageContent() {
     return(
+        <>
         <section className = "bg-[#FAFAFA]">
         <div className = "py-20 gap-y-12 mx-10 md:mx-60">
             <div className = "flex flex-col text-center gap-y-2.5">
@@ -13,5 +15,16 @@ export function PageContent() {
             </div>
         </div>
         </section>
+        <section className = "py-20 mx-10 md:mx-60">
+            <div className = "flex flex-col text-center items-center gap-y-2.5">
+                <h4 className = "flex flex-wrap font-normal text-xl text-[#737373]">Featured Products</h4>
+                <h3 className = "flex flex-wrap justify-center w-[15rem] mont font-bold text-2xl text-[#252B42]">BESTSELLER PRODUCTS</h3>
+                <p className = "flex flex-wrap justify-center w-[16rem] font-normal text-[#737373]">Problems trying to resolve the conflict between </p>
+            </div>
+            <div className = "">
+                <ProductCardsGroup/>
+            </div>
+        </section>
+        </>
     )
 }
