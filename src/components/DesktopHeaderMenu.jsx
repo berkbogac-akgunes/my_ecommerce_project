f04dea40e2d6003048137ae0e8a3efdf684ac36a
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-10-22 17:18:55 +0300

    T2: Home Page Fixes #2

--- src/components/DesktopHeaderMenu.jsx
+++ src/components/DesktopHeaderMenu.jsx
@@ -30,8 +30,8 @@ export function DesktopHeaderMenu() {
                 <h3 className = "font-bold text-2xl">BB Butik</h3>
             </div>
             <div className = "font-bold flex ml-24 mr-56 gap-x-10">
-                <NavLink to="/home">Home</NavLink>
-                <NavLink className = "font-semibold" to="">Shop</NavLink>
+                <NavLink to="/">Home</NavLink>
+                <NavLink className = "font-semibold" to="/shop">Shop</NavLink>
                 <NavLink to="">About</NavLink>
                 <NavLink to="">Blog</NavLink>
                 <NavLink to="">Contact</NavLink>
