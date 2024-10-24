571746be24f812e13aefbe21ce85dc0a873a2f40
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-10-24 22:11:06 +0300

    contact fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/productdetail">
           <ProductPage />
         </Route>
-        <Route path="/contact">
+        <Route path="/contactUs">
           <ContactPage />
         </Route>
       </Switch>
