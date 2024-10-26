87cafa3583525e48af166a68f3885e086e5029ff
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-10-26 20:40:16 +0300

    add about us page

---
+++ src/components/AboutUsBrands.jsx
@@ -0,0 +1,15 @@
+import { ShopPageBrands } from "./ShopPageBrands";
+
+export function AboutUsBrands() {
+    return(
+        <>
+        <section className = "">
+        <div className = "flex flex-col text-center items-center">
+        <h2 className = "md:w-full px-24 bg-[#FAFAFA] font-bold text-5xl pt-32 pb-10">Big Companies Are Here</h2>
+        <p className = "md:w-full md:px-[30rem] px-12 mont bg-[#FAFAFA] text-[#737373] text-sm font-semibold">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
+        </div>
+        <ShopPageBrands/>
+        </section>
+        </>
+    )
+}
---
+++ src/components/AboutUsWork.jsx
@@ -0,0 +1,26 @@
+export function AboutUsWork() {
+    return(
+        <>
+        <section className = "md:items-center md:flex md:flex-row md:px-0 px-12 h-[40rem] bg-[#2A7CC7]">
+        <div className = "pt-32 md:pt-0 w-[100%] flex flex-col items-center text-center mont">
+            <div className = "md:flex md:flex-col md:items-left md:text-left">
+            <h4 className = "font-bold text-lg mb-9 text-white">WORK WITH US</h4>
+            <h2 className = "font-bold md:text-6xl md:text-5xl text-5xl mb-9 text-white">Now Let’s grow Yours</h2>
+            <h4 className = "flex flex-wrap font-semibold text-xl md:text-base mb-9 md:w-[34rem] w-[22rem] text-white">The gradual accumulation of information about atomic and 
+            small-scale behavior during the first quarter of the 20th </h4>
+            <div className = "flex md:justify-start justify-center gap-y-6 ">
+            <button className = "border-white border bg-[#2A7CC7] text-white py-3 w-[10rem] rounded font-semibold text-lg">Button</button>
+            </div>
+            </div>
+            </div>
+        <div className = "max-sm:hidden">
+            <img
+            className = "md:h-[40rem] w-[65rem] object-cover"
+            src = "https://s3-alpha-sig.figma.com/img/a014/4955/2eeaef7ecedd3954687aefbdb6236bb6?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MR8YCCKHPQP1hpNykEGO-wgog~Lyg11ScqKfItChVlhptA2SFjpMl-0rvFRXtWG9TP-gqIc-HpEo2MOrB3F2ZqtRnIwACzTSnSqLvgiFYmU6Oaov1iicvah9rOehO3oDJlILDjaaOwR~RM1Zw5AKPkbAKYdnBelkX740a~S~qI4SSwFO8UTUHwSdml04cIKyacGvmLYrE~5hMI9Ux3FV6VtolIiqItXR6XWgU-korV6GvjjArzXEEWRrPBji0Ot3ojlF9oHPnqX70CRue~N~-V~2188stEoNHyjMpM6DvFQYgVkN5jzooOVOH8AJrjK9E1z77rsTz~SVAP-RA2Z~MA__"
+            title = "AboutUsWorkImage"
+            />
+        </div>
+        </section>
+        </>
+    )
+}
