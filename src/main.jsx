18b804f8255ea90e33e751a5966a4ea20ddd3605
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-10-29 17:12:14 +0300

    add redux, actions-reducers-store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import { store } from './store/store.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
+  <Provider store = {store}>
   <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
   </BrowserRouter>
+  </Provider>
 )
